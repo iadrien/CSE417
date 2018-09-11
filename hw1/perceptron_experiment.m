30c01955877ef507f280584794d76442abeec5c0
iadrien <xie.z@wustl.edu>
2018-09-11 01:19:20 -0500

    HW1

--- hw1/perceptron_experiment.m
+++ hw1/perceptron_experiment.m
@@ -8,6 +8,41 @@ function [ num_iters bounds] = perceptron_experiment ( N, d, num_samples )
 %               and the actual number of iterations
 %      (both the outputs should be num_samples long)
 
- 
+    data = [];
+    dataWOLabel = [];
+    num_iters = [];
+    p = 0;
+    r = 0;
+    bounds = [];
+    
+%for each itereation, an experiment of 100 sample is performed
+%for convenience, w is first set to 10 dimension vector 
+    for i=1:num_samples
+        w = rand(1,d);
+    
+        for j=1:N
+            x = rand(1,d)*2-1;
+            y = sign(dot(w,x));
+            temp = [x y];
+            data = [data; temp];
+            dataWOLabel = [dataWOLabel; x];
+        end
+        
+        [weightVector, iteration] = perceptron_learn(data);
+        p = min(abs(weightVector*transpose(dataWOLabel)));
+        r = norm(norm(dataWOLabel,Inf));
+        
+        num_iters = [num_iters iteration];
+        data = [];
+        dataWOLabel = [];
+        
+        bound = (r * norm(weightVector)/ p).^2;
+        bounds = [bounds bound];
+    end
+    figure;
+    logdif = log(bounds-num_iters);
+    histogram(logdif);
+    figure;
+    histogram(num_iters);
 end
 
--- hw1/perceptron_learn.m
+++ hw1/perceptron_learn.m
@@ -6,6 +6,39 @@ function [ w iterations ] = perceptron_learn( data_in )
 %   Outputs: w: A weight vector (should linearly separate the data if it is
 %               linearly separable)
 %            iterations: The number of iterations the algorithm ran for
+    w=[0 0 0 0 0 0 0 0 0 0 ];
+    iterations = 0;
+    
 
+    while true   
+        mc = missClassified(data_in,w);
+        
+        if size(mc)==0
+            break;
+        end
+        [r1, r2]=size(mc);
+        index = randi([1 r1],1,1);
+
+        x = mc(index,:);
+        y = x(r2);
+        wx = sign(dot(w,x(1:r2-1)));
+        
+        w = w + y*x(1:r2-1);
+        iterations = iterations + 1;
+    end
+end
+
+function [missSet] = missClassified (data_in, w)
+    missSet = [];
+    [m, n]=size(data_in);
+
+    for index = 1:m   
+        x = data_in(index,:);
+        y = x(n);
+        wx = sign(dot(w,x(1:n-1)));  
+        if wx ~= y
+            missSet=[missSet; x];
+        end
+    end
 end
 
